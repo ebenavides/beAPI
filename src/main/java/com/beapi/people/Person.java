bb0e5574cc19c00733608396b8f2c3a041667aaa
Esteban Benavides <ebenavidescruz@gmail.com>
2018-08-07 22:06:30 -0600

    Hibernate annotations

--- src/main/java/com/beapi/people/Person.java
+++ src/main/java/com/beapi/people/Person.java
@@ -1,7 +1,12 @@
 package com.beapi.people;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 	
+	@Id
 	private int id;
 	private String name;
 	private String lastname;
