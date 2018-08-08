c7e0e02f2edc25da93fffee62a1539e3ad838b47
Esteban Benavides <ebenavidescruz@gmail.com>
2018-08-07 21:44:27 -0600

    Initial commit

---
+++ src/main/java/com/beapi/BeApiApplication.java
@@ -0,0 +1,12 @@
+package com.beapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApiApplication.class, args);
+	}
+}
