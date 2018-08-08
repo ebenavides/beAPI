035612739ef89b6fc538746966cc7567f555780e
Esteban Benavides <ebenavidescruz@gmail.com>
2018-08-07 22:54:30 -0600

    defining queries

--- src/main/java/com/beapi/people/APIService.java
+++ src/main/java/com/beapi/people/APIService.java
@@ -1,13 +1,33 @@
 package com.beapi.people;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class APIService {
 	
-	public void getPerson() {}
+	@Autowired
+	private PersonRepository peopleRepository;
 	
-	public void getAllPeople() {}
+	
+	/*Get Person by ID*/
+	public Person getPerson(String id) {
+		Optional<Person> person = peopleRepository.findById(id);
+		if (person.isPresent()){return person.get();}
+		else{return new Person();}
+	}
+	
+	/*Get Collection*/
+	public List<Person> getAllPeople() {
+		List<Person> people = new ArrayList<>();
+		peopleRepository.findAll()
+		.forEach(people::add);
+		return people;
+	}
 	
 	public void orderByLastname() {}
 	
