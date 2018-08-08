57dd2d809c2d5cbe351ed65518c1efb4569062e1
Esteban Benavides <ebenavidescruz@gmail.com>
2018-08-07 22:32:35 -0600

    defining responses on Controller

--- src/main/java/com/beapi/people/Controller.java
+++ src/main/java/com/beapi/people/Controller.java
@@ -1,13 +1,43 @@
 package com.beapi.people;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
 	@Autowired
-	private APIService apiservice;
+	private APIService apiService;
+	
+	
+	/*GET - All*/
+	@RequestMapping("/people")
+	public void getAllPeople(){
+		/* return apiService.getAllPeople();*/
+	}
+	
+	/*GET - One*/
+	@RequestMapping("/people/{id}")
+	public void getPerson() {
+		/*return apiService.getPerson(@PathVariable("id") String id);*/
+	}
+	
+	/*PUT - Lastname*/
+	@RequestMapping(method=RequestMethod.PUT, value="/people/{id}")
+	public void updateLastname() {
+		/* apiService.updateLastname();*/
+	}
+	
+
+	/*DELETE - One By SSN*/
+	@RequestMapping(method=RequestMethod.DELETE, value="/people/{ssn}")
+	public void deleteBySSN() {
+		/* apiService.deleteBySSN();*/
+	}
+	
+	
 	
 
 }
